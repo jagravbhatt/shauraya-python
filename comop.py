481f33a1faf91806c33167258adf25a45a9304d0
Jagrav Bhatt <your_email@gmail.com>
2026-06-10 19:28:56 +0530

    shaurya_python

---
+++ comop.py
@@ -0,0 +1,11 @@
+#>,< >=/,<=,==,<,!=
+
+a=5
+b=20
+
+print(a>b)#False
+print(a<b)#True
+print(a<=b)#True
+print(a!=b)#True
+print(a==b)#False
+print(a>=b)#False
---
+++ operators.py
@@ -0,0 +1,11 @@
+a=10
+b=6
+
+
+print(a+b)#15
+print(a-b)#5
+print(a*b)#50
+print(a/b)#2.0
+print(a%b)#0
+print(a**b)#10000000000
+[print(a//b)]#1
